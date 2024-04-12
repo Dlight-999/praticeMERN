61c32ab65a83dd0dace123b537121456811e417c
D <dracokd19@gmail.com>
2024-04-12 14:31:13 +0545

    Fixed adding workout

--- frontend/src/components/AddWorkout.tsx
+++ frontend/src/components/AddWorkout.tsx
@@ -1,29 +1,69 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
 
+interface IWorkout{
+    name: string;
+    sets: string;
+    reps:string;
+}
 function AddWorkout() {
+    const[workout, setWorkout]=useState<IWorkout>({
+        name:'',
+        sets:'',
+        reps:'',
+    });
+    const[err,setErr]= useState<string>('');
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>)=>{
+        const{name, value} = e.target;
+        setWorkout(prevState =>({
+            ...prevState,
+            [name]:value
+        }))
+    }
+
+    const handleSubmit = async (e:React.FormEvent<HTMLFormElement>)=>{
+        e.preventDefault();
+        try{
+            await axios.post('http://localhost:5000/api/workout', workout);
+            setWorkout({
+                name:'',
+                sets:'',
+                reps:''
+            })
+
+            alert('Workout Added Successfully');
+        }
+        catch(error:any){
+            console.error('Error adding:',error);
+            setErr(error.response.data.error)
+        }
+    }
   return (
    <>
     <div className="form w-full flex flex-col justify-center items-center">
                    <div className='bg-black rounded-lg text-white w-auto p-5'> 
                     Add Workout
+                    <form onSubmit={handleSubmit}>
                     <div className='flex flex-col'>
                         <div className='flex flex-col'>
                             <label htmlFor="">exercise</label>
-                            <input type="text" name="name" id="name" className='rounded-md h-12 text-black p-2'  />
+                            <input type="text" name="name" id="name" className='rounded-md h-12 text-black p-2' value={workout.name} onChange={handleInputChange}/>
                         </div>
                         <div className='flex my-6 justify-between gap-5'>
                            <div className='flex flex-col'>
                            <label htmlFor="">Sets</label>
-                            <input type="number" name="sets" id="sets" className='[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none rounded-md h-12 text-black p-2' min='0'/>
+                            <input type="number" name="sets" id="sets" className='[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none rounded-md h-12 text-black p-2' value={workout.sets} onChange={handleInputChange}  min='0'/>
                            </div>
                            <div className='flex flex-col'>
                            <label htmlFor="">Reps</label>
-                            <input type="number" name="reps" id="reps" className='[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none rounded-md h-12 text-black p-2'  min='0'/>
+                            <input type="number" name="reps" id="reps" className='[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none rounded-md h-12 text-black p-2' value={workout.reps} onChange={handleInputChange}  min='0'/>
                            </div>
                         </div>
+                        {err && <div>{err}</div>}
                         <button className='my-5 bg-sky-600 rounded-md p-2 hover:bg-indigo-800 text-lg font-bold' type="submit">Add</button>
 
                     </div>
+                    </form>
                 </div>
                 </div>
    </>
--- frontend/src/components/GetWorkout.tsx
+++ frontend/src/components/GetWorkout.tsx
@@ -23,9 +23,9 @@ const GetWorkout:React.FC=()=> {
     };
   return (
     <>
-        <div className="flex w-full h-full flex-col items-center py-5 mx-2 ">
+        <div className="flex w-full h-full flex-col items-center py-5 mx-2 overflow-auto">
             {workout.map((workouts)=>(
-                <div className="card flex flex-col border-b-2 border-black bg-white rounded-lg shadow-xl shadow-gray-600 border text-black p-5 w-full my-2 mx-10 text-lg">
+                <div className="card flex flex-col border-b-2 border-black bg-white rounded-lg shadow-xl shadow-gray-600 border text-black p-5 w-full my-2 mx-10 text-lg" key={workouts._id} >
                 <div className=''><h2 className='font-semibold'>Exercise:</h2>  {workouts.name}</div>
                 <div className='flex w-full justify-between items-start'>
                     <div className='flex justify-between w-full bg-gray-400 m-2 px-2 rounded-lg border-b-2 border-black shadow-md shadow-gray-600'><h2 className='font-semibold'>Sets:</h2> {workouts.sets}</div>
